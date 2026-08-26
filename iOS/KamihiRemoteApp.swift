c78ef6ca86dc00b6b5282bf2937608d9e22d7886
Michał Bożek <80297855+kjm9198@users.noreply.github.com>
2026-08-26 02:18:21 +0200

    feat: route app through connection doctor shell

--- iOS/KamihiRemoteApp.swift
+++ iOS/KamihiRemoteApp.swift
@@ -6,7 +6,7 @@ struct KamihiRemoteApp: App {
 
     var body: some Scene {
         WindowGroup {
-            KamihiPolishedRootView()
+            KamihiAppShell()
                 .environmentObject(session)
                 .preferredColorScheme(.dark)
                 .statusBarHidden(true)
