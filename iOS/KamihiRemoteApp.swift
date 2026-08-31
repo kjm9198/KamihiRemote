3452e0b97765b5beac91a8339f5bd77f80057477
Michał Bożek <80297855+kjm9198@users.noreply.github.com>
2026-09-01 00:55:08 +0200

    Wire desktop recovery and health lifecycle

--- iOS/KamihiRemoteApp.swift
+++ iOS/KamihiRemoteApp.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct KamihiRemoteApp: App {
     @StateObject private var session = RemoteSession()
     @StateObject private var desktop = DesktopSession.shared
+    @StateObject private var desktopRecovery = DesktopRecoveryCoordinator.shared
 
     init() {
         #if DEBUG
@@ -19,8 +20,19 @@ struct KamihiRemoteApp: App {
             AdvancedDesktopAwareRootView()
                 .environmentObject(session)
                 .environmentObject(desktop)
+                .environmentObject(desktopRecovery)
                 .preferredColorScheme(.dark)
                 .statusBarHidden(false)
+                .onChange(of: desktop.isExternalDisplayConnected) { _, connected in
+                    if connected {
+                        _ = desktopRecovery.prepareForConnection(desktop: desktop)
+                    } else {
+                        desktopRecovery.finishSession(desktop: desktop)
+                    }
+                }
+                .onChange(of: desktop.windows) { _, _ in
+                    desktopRecovery.autosave(desktop: desktop)
+                }
         }
     }
 }
