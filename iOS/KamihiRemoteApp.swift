e941ec2b707e6f31fbfb0f76304938d8e68f5bab
Michał Bożek <80297855+kjm9198@users.noreply.github.com>
2026-08-31 23:55:51 +0200

    test: run desktop self checks in debug builds

--- iOS/KamihiRemoteApp.swift
+++ iOS/KamihiRemoteApp.swift
@@ -5,6 +5,15 @@ struct KamihiRemoteApp: App {
     @StateObject private var session = RemoteSession()
     @StateObject private var desktop = DesktopSession.shared
 
+    init() {
+        #if DEBUG
+        Task { @MainActor in
+            _ = GestureEngineTests.runSelfChecks()
+            _ = DesktopServicesTests.runSelfChecks()
+        }
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             ProductivityDesktopAwareRootView()
